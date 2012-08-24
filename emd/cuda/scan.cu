9a2f831b6a689f6031c14790dc78b2818fc4a1d8
David JH Shih <djh.shih@gmail.com>
2012-08-24 18:33:39 -0400

    Added matrix support for prescan. Unknown bug persists for matrtices.

--- emd/cuda/scan.cu
+++ emd/cuda/scan.cu
@@ -16,28 +16,28 @@ typedef float real_t;
 int main(int argc, char* argv[]) {
 	
 	real_t *h_x, *h_y;
-	//real_t *h_block_x, *h_block_y;
+	real_t *h_block_x, *h_block_y;
 	real_t *d_x, *d_y, *d_block_x, *d_block_y;
 	
 	// assume N < 2^22
 	// since O(N) shared memory is used, the application will likely be memory-bound
 	
-	const size_t N = 2048;
-	const size_t m = 4;
+	const size_t N = 64;
+	const size_t m = 9;
 	
-	size_t elemPerBlock = 128;
+	size_t elemPerBlock = 4;
 	
 	dim3 block_dim = elemPerBlock / 2;
 	dim3 grid_dim = N / elemPerBlock + (N % elemPerBlock == 0 ? 0 : 1);
 	
-	size_t nbytes = N * sizeof(real_t);
-	size_t nbytes_block = grid_dim.x * sizeof(real_t);
+	size_t nbytes = N * sizeof(real_t) * m;
+	size_t nbytes_block = grid_dim.x * sizeof(real_t) * m;
 	
 	// allocate array on host
 	h_x = (real_t*)malloc(nbytes);
 	h_y = (real_t*)malloc(nbytes);
-	//h_block_x = (real_t*)malloc(nbytes_block);
-	//h_block_y = (real_t*)malloc(nbytes_block);
+	h_block_x = (real_t*)malloc(nbytes_block);
+	h_block_y = (real_t*)malloc(nbytes_block);
 	
 	
 	// allocate array on device
@@ -47,49 +47,54 @@ int main(int argc, char* argv[]) {
 	cudaMalloc((void**) &d_block_y, nbytes_block);
 	
 	// initialize host array
-	for (size_t i = 0; i < N; ++i) {
-		h_x[i] = rand() % 10;
+	for (size_t i = 0; i < N*m; ++i) {
+		h_x[i] = (rand() % 3);
 	}
 	
 	// copy data to device
 	cudaMemcpy(d_x, h_x, nbytes, cudaMemcpyHostToDevice);
 	
-	ScalarAdder<real_t> adder;
-	ScalarSetter<real_t> setter;
-	real_t identity(0);
+	//ScalarAdder<real_t> adder;
+	//ScalarSetter<real_t> setter;
+	ArrayAdder<real_t> adder(m);
+	ArraySetter<real_t> setter(m);
+	//Matrix33MultiplerPre<real_t> adder;
+	//Matrix33Setter<real_t> setter;
+	//MatrixMultipler<real_t> adder(3);
+	//MatrixSetter<real_t> setter(3);
 	
 	// do calculation on device
 	
 	// elements are divided into blocks
 	// each thread processes two elements within a block
-	prescan <<< grid_dim, block_dim, elemPerBlock*sizeof(real_t) >>> (elemPerBlock, d_x, d_y, identity, adder, setter);
+	prescan <<< grid_dim, block_dim, elemPerBlock*sizeof(real_t) * m >>> (elemPerBlock, d_x, d_y, adder, setter, m);
 	
 	// one block; each thread processes a scan block from above
-	aggregate_block_sum <<< 1, grid_dim >>> (elemPerBlock, d_y, d_block_x, setter);
+	aggregate_block_sum <<< 1, grid_dim >>> (elemPerBlock, d_y, d_block_x, setter, m);
 	
 	// one block; each thread processes two scan block sums (hence need half the number of scan blocks from previous run)
-	prescan <<< 1, grid_dim.x/2, grid_dim.x*sizeof(real_t) >>> (grid_dim.x, d_block_x, d_block_y, identity, adder, setter);
+	prescan <<< 1, grid_dim.x/2, grid_dim.x*sizeof(real_t) * m >>> (grid_dim.x, d_block_x, d_block_y, adder, setter, m);
 	
 	// each thread processes one element in original data
 	// need twice as many blocks as before, since each thread now processes one element
-	add_block_cumsum <<< grid_dim.x*2, block_dim >>> (N, d_block_y, d_y, adder, setter);
+	add_block_cumsum <<< grid_dim.x*2, block_dim >>> (N, d_block_y, d_y, adder, setter, m);
 
 	// retrieve results from device and store it in host array
 	cudaMemcpy(h_y, d_y, nbytes, cudaMemcpyDeviceToHost);
-	//cudaMemcpy(h_block_x, d_block_x, nbytes_block, cudaMemcpyDeviceToHost);
-	//cudaMemcpy(h_block_y, d_block_y, nbytes_block, cudaMemcpyDeviceToHost);
+	cudaMemcpy(h_block_x, d_block_x, nbytes_block, cudaMemcpyDeviceToHost);
+	cudaMemcpy(h_block_y, d_block_y, nbytes_block, cudaMemcpyDeviceToHost);
 	
 	// compute gold standard
-	real_t* h_gold = new real_t[N];
+	real_t* h_gold = new real_t[N*m];
 	setter(h_gold[0], h_x[0]);
 	for (size_t i = 1; i < N; ++i) {
-		setter(h_gold[i], h_gold[i-1]);
-		adder(h_gold[i], h_x[i]);
+		setter(h_gold[i*m], h_x[i*m]);
+		adder(h_gold[i*m], h_gold[(i-1)*m]);
 	}
 	
 	// print results
 	bool equal = true;
-	for (size_t i = 0; i < N; ++i) {
+	for (size_t i = 0; i < N*m; ++i) {
 		printf("%d %.0f %.0f %.0f", i, h_x[i], h_y[i], h_gold[i]);
 		if (std::abs(h_y[i] - h_gold[i]) > 1e-5) {
 			equal = false;
@@ -100,11 +105,9 @@ int main(int argc, char* argv[]) {
 	}
 	if (!equal) printf("Differences detected!\n");
 	
-	/*
-	for (size_t i = 0; i < grid_dim.x; ++i) {
+	for (size_t i = 0; i < grid_dim.x*m; ++i) {
 		printf("%d %.0f %.0f\n", i, h_block_x[i], h_block_y[i]);
 	}
-	*/
 	
 	cudaError_t code = cudaGetLastError();
 	if (code != cudaSuccess) {
@@ -115,8 +118,8 @@ int main(int argc, char* argv[]) {
 	// clean up
 	free(h_x);
 	free(h_y);
-	//free(h_block_x);
-	//free(h_block_y);
+	free(h_block_x);
+	free(h_block_y);
 	
 	delete [] h_gold;
 	
--- emd/cuda/scan_kernel.hpp
+++ emd/cuda/scan_kernel.hpp
@@ -4,10 +4,15 @@
 #define CONFLICT_FREE_OFFSET(n)  ((n) >> NUM_BANKS + (n) >> (2 * LOG_NUM_BANKS))  
 #include <boost/concept_check.hpp>
 
+
 template <typename T>
 struct ScalarAdder {
 	__device__ __host__ void operator()(T& a, const T& b) const {
 		a += b;
+	}
+	// set a to identity
+	__device__ __host__ void operator()(T& a) const {
+		a = 0;
 	}
 };
 
@@ -20,15 +25,20 @@ struct ScalarSetter {
 
 template <typename T>
 struct ArrayAdder {
-	ArrayAdder(size_t n): _n(n) {}
+	ArrayAdder(size_t n) : _n(n) {}
 	__device__ __host__ void operator()(T& a, const T& b) const {
-		T *pa = &a, *pb = &b;
+		T *pa = &a;
+		const T *pb = &b;
 		for (size_t i = 0; i < _n; ++i) {
 			*(pa++) += *(pb++);
 		}
 	}
-	private:
-		size_t _n;
+	// set to identity
+	__device__ __host__ void operator()(T& a) const {
+		memset(&a, 0, sizeof(T)*_n);
+	}
+private:
+	size_t _n;
 };
 
 template <typename T>
@@ -41,9 +51,114 @@ struct ArraySetter {
 		size_t _n;
 };
 
+/**
+ * Squared matrix mulitplier
+ */
+template <typename T>
+struct MatrixMultipler {
+	MatrixMultipler(size_t n) : _n(n) {}
+	__device__ __host__ void operator()(T& a, const T& b) const {
+		T* pa = &a;
+		const T* pb = &b;
+		T* pc = new T[_n*_n];
+		memset(pc, 0, sizeof(T)*_n*_n);
+		for (size_t i = 0; i < _n; ++i) {
+			for (size_t k = 0; k < _n; ++k) {
+				for (size_t j = 0; j < _n; ++j) {
+					pc[i*_n+j] += pa[i*_n+k]*pb[k*_n+j];
+				}
+			}
+		}
+		memcpy(pa, pc, sizeof(T)*_n*_n);
+		delete [] pc;
+	}
+	// set to identity
+	__device__ __host__ void operator()(T& a) const {
+		T* pa = &a;
+		memset(pa, 0, sizeof(T)*_n*_n);
+		for (size_t i = 0; i < _n; ++i) {
+			pa[i*_n + i] = (T)1;
+		}
+	}
+private:
+	size_t _n;
+};	
 
+template <typename T>
+struct MatrixSetter {
+	MatrixSetter(size_t n) : _n(n) {}
+	__device__ __host__ void operator()(T& a, const T& b) const {
+		memcpy(&a, &b, sizeof(T)*_n*_n);
+	}
+private:
+	size_t _n;
+};	
+
+/**
+ * Mulitpler for 3x3 matrix
+ */
+template <typename T>
+struct Matrix33MultiplerPost {
+	__device__ __host__ void operator()(T& a, const T& b) const {
+		T* pa = &a;
+		const T* pb = &b;
+		T pc[9];
+		memset(pc, 0, sizeof(T)*9);
+		for (size_t i = 0; i < 3; ++i) {
+			for (size_t k = 0; k < 3; ++k) {
+				for (size_t j = 0; j < 3; ++j) {
+					pc[i*3+j] += pa[i*3+k]*pb[k*3+j];
+				}
+			}
+		}
+		memcpy(pa, pc, sizeof(T)*9);
+	}
+	// set to identity
+	__device__ __host__ void operator()(T& a) const {
+		T* pa = &a;
+		memset(&pa[1], 0, sizeof(T)*7);
+		//pa[1] = pa[2] = pa[3] = pa[5] = pa[6] = pa[7] = 0;
+		pa[0] = pa[4] = pa[8] = 1;
+	}
+};
+
+template <typename T>
+struct Matrix33MultiplerPre {
+	__device__ __host__ void operator()(T& a, const T& b) const {
+		T* pa = &a;
+		const T* pb = &b;
+		T pc[9];
+		memset(pc, 0, sizeof(T)*9);
+		for (size_t i = 0; i < 3; ++i) {
+			for (size_t k = 0; k < 3; ++k) {
+				for (size_t j = 0; j < 3; ++j) {
+					pc[i*3+j] += pb[i*3+k]*pa[k*3+j];
+				}
+			}
+		}
+		memcpy(pa, pc, sizeof(T)*9);
+	}
+	// set to identity
+	__device__ __host__ void operator()(T& a) const {
+		T* pa = &a;
+		memset(&pa[1], 0, sizeof(T)*7);
+		//pa[1] = pa[2] = pa[3] = pa[5] = pa[6] = pa[7] = 0;
+		pa[0] = pa[4] = pa[8] = 1;
+	}
+};
+
+template <typename T>
+struct Matrix33Setter {
+	__device__ __host__ void operator()(T& a, const T& b) const {
+		memcpy(&a, &b, sizeof(T)*9);
+	}
+};
+
+/**
+ * NB  If the binary operation is not communicative, be sure to provide a prefix binary operator.
+ */
 template <typename T, typename BinaryOp, typename Setter >
-__global__ void prescan(size_t n, const T* x, T* y, T identity, BinaryOp binaryOp, Setter copy) {
+__global__ void prescan(const size_t n, const T* x, T* y, BinaryOp binaryOp, Setter copy, const size_t m=1) {
 	extern __shared__ T shared[];
 	
 	size_t i = threadIdx.x;
@@ -55,8 +170,8 @@ __global__ void prescan(size_t n, const T* x, T* y, T identity, BinaryOp binaryO
 	size_t bi = i + (n/2);
 	size_t bankOffsetA = CONFLICT_FREE_OFFSET(ai);
 	size_t bankOffsetB = CONFLICT_FREE_OFFSET(bi);
-	copy(shared[ai + bankOffsetA], x[ai + blockOffset]);
-	copy(shared[bi + bankOffsetB], x[bi + blockOffset]);
+	copy(shared[(ai + bankOffsetA)*m], x[(ai + blockOffset)*m]);
+	copy(shared[(bi + bankOffsetB)*m], x[(bi + blockOffset)*m]);
 	
 	size_t offset = 1;
 
@@ -68,13 +183,15 @@ __global__ void prescan(size_t n, const T* x, T* y, T identity, BinaryOp binaryO
 			size_t bi = offset * (2*i+2) - 1;
 			ai += CONFLICT_FREE_OFFSET(ai);
 			bi += CONFLICT_FREE_OFFSET(bi);
-			binaryOp(shared[bi], shared[ai]);
+			binaryOp(shared[bi*m], shared[ai*m]);
 		}
 		offset *= 2;
 	}
 	
 	// replace the last element with identity (to be propagated back to position 0)
-	if (i == 0) copy(shared[n-1 + CONFLICT_FREE_OFFSET(n-1)], identity);
+	if (i == 0) binaryOp(shared[(n-1 + CONFLICT_FREE_OFFSET(n-1))*m]);
+	
+	//T* t = new T[m];
 	
 	// traverse down tree and build scan
 	for (size_t d = 1; d < n; d *= 2) {
@@ -85,12 +202,17 @@ __global__ void prescan(size_t n, const T* x, T* y, T identity, BinaryOp binaryO
 			size_t bi = offset * (2*i+2) - 1;
 			ai += CONFLICT_FREE_OFFSET(ai);
 			bi += CONFLICT_FREE_OFFSET(bi);
-			T t;
-			copy(t, shared[ai]);
-			copy(shared[ai], shared[bi]);
-			binaryOp(shared[bi], t);
+			//T t;
+			T t[10];
+			copy(*t, shared[ai*m]);
+			copy(shared[ai*m], shared[bi*m]);
+			// reverse order
+			binaryOp(*t, shared[bi*m]);
+			copy(shared[bi*m], *t);
 		}
 	}
+	
+	//delete [] t;
 	
 	__syncthreads();
 	
@@ -100,38 +222,38 @@ __global__ void prescan(size_t n, const T* x, T* y, T identity, BinaryOp binaryO
 	int j = ai + blockOffset - 1;
 	
 	if (j >= 0) {
-		copy(y[j], shared[ai + bankOffsetA]);
+		copy(y[j*m], shared[(ai + bankOffsetA)*m]);
 		// thread writing to second last element of block also writes result for last element
 		++j;
 		if ((j+1) % n == 0) {
-			copy(y[j], shared[ai + bankOffsetA]);
-			binaryOp(y[j], x[j]);
+			copy(y[j*m], shared[(ai + bankOffsetA)*m]);
+			binaryOp(y[j*m], x[j*m]);
 		}
 	}
 
 	j = bi + blockOffset - 1;
 	if (j >= 0) {
-		copy(y[j], shared[bi + bankOffsetB]);
+		copy(y[j*m], shared[(bi + bankOffsetB)*m]);
 		// thread writing to second last element of block also writes result for last element
 		++j;
 		if ((j+1) % n == 0) {
-			copy(y[j], shared[bi + bankOffsetB]);
-			binaryOp(y[j], x[j]);
+			copy(y[j*m], shared[(bi + bankOffsetB)*m]);
+			binaryOp(y[j*m], x[j*m]);
 		}
 	}
 }
 
 template <typename T, typename Setter>
-__global__ void aggregate_block_sum(size_t block_size, const T* y, T* out, Setter copy) {
-	copy(out[threadIdx.x], y[(threadIdx.x+1) * block_size - 1]);
+__global__ void aggregate_block_sum(size_t block_size, const T* y, T* out, Setter copy, size_t m = 1) {
+	copy(out[threadIdx.x*m], y[((threadIdx.x+1) * block_size - 1)*m]);
 }
 
 template <typename T, typename BinaryOp, typename Setter >
-__global__ void add_block_cumsum(size_t n, const T* blocks, T* y, BinaryOp binaryOp, Setter copy) {
+__global__ void add_block_cumsum(size_t n, const T* blocks, T* y, BinaryOp binaryOp, Setter copy, size_t m = 1) {
 	// since inclusive scan was calculated, don't modify elements within first block
 	size_t i = blockIdx.x * blockDim.x + threadIdx.x;
 	size_t j = i / (2*blockDim.x);
 	if (j > 0) {
-		binaryOp(y[i], blocks[j-1]);
+		binaryOp(y[i*m], blocks[(j-1)*m]);
 	}
 }
